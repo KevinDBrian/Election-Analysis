3d4a00145a35b22363e8d5fa1b2b847d7df296ca
KevinDBrian <95155176+KevinDBrian@users.noreply.github.com>
2022-01-14 10:22:20 -0600

    Add files via upload
    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -23,6 +23,13 @@ candidate_options = []
 # Candidate votes (dict)
 candidate_votes = {}
 
+# Winning candidate and winning count tracker
+# Declare winning candidate, count, and percentage variables
+    # Winning candidate variable declared with empty STRING tracker (variable = "") since its a name
+winning_candidate = ""
+winning_count = 0
+winning_perc = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -48,22 +55,39 @@ with open(file_to_load) as election_data:
         candidate_votes[candidate_name] += 1
 
 # Print the total votes
-print(candidate_votes)
+# print(candidate_votes)
 
 # Close the file.
 election_data.close()
 
-# Calcualte vote percentages for each candidate
-# Loop through the counts
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
 for candidate_name in candidate_votes:
-
-    # Define votes and retrive counts
+    # Retrieve vote count of a candidate.
     votes = candidate_votes[candidate_name]
+    # Calculate the percentage of votes.
+    vote_perc = (votes / total_votes) * 100
 
-    # Calculate percentage
-    vote_percentage = (votes / total_votes) * 100
+    #  To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
 
-    # Print each candidate's vote percentages
-    print(f"{candidate_name} recieved {vote_percentage:.2f}% of the vote!")
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_perc > winning_perc):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_perc = vote_perc
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
 
-# start at 3.5.5
+#  To do: print out the winning candidate, vote count and percentage to terminal.
+    print(f"\n{candidate_name}: {vote_perc:.1f}% ({votes:,})\n")
+
+win_sum = (
+    f"----------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_perc:.1f}%\n"
+    f"----------------------------\n")
+print(win_sum)
